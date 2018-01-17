2eb15833928340c4e9019b5322ab8da3bf3a1bff
Asif Malik <asif.malik@gmail.com>
2018-01-17 10:01:32 +0000

    Transfer

--- src/fi/iki/jka/JPhotoFrame.java
+++ src/fi/iki/jka/JPhotoFrame.java
@@ -438,7 +438,7 @@ public class JPhotoFrame extends JFrame
                     JOptionPane.showMessageDialog(this, "File not set");
                     return;
                 }
-                boolean status = photos.exportSlideshow(targetFile, 1);
+                boolean status = photos.exportSlideshESow(targetFile, 1);
                 if (status==false)
                     JOptionPane.showMessageDialog(this, "Export of slideshow to "+albumFileName+ " failed.",
                                                   APP_NAME, JOptionPane.ERROR_MESSAGE);
@@ -546,7 +546,7 @@ public class JPhotoFrame extends JFrame
         else if (cmd.equals(JPhotoMenu.A_WATERMARK)) {
             String def = photos.getWatermark();
             if (def.equals(""))
-                def = "© "+Calendar.getInstance().get(Calendar.YEAR)+" ";
+                def = "ï¿½ "+Calendar.getInstance().get(Calendar.YEAR)+" ";
             String res = JOptionPane.showInputDialog(this, "Watermark",
                                                      def);
             if (res!=null)
@@ -596,7 +596,7 @@ public class JPhotoFrame extends JFrame
             JOptionPane.showMessageDialog(this, APP_NAME+" v1.4.5 - Organize and Publish Your Digital Photos.\n"+
                                           "Copyright 2005-2007 Jari Karjala [www.jpkware.com],\n"
                                           +"Tarja Hakala [www.hakalat.net]"
-                                          +" and Zbynek Mužík [zbynek.muzik@email.cz]\n"
+                                          +" and Zbynek Muï¿½ï¿½k [zbynek.muzik@email.cz]\n"
                                           +"This is free software, licenced under the GNU General Public License.",
                                           JPhotoMenu.A_ABOUT, JOptionPane.INFORMATION_MESSAGE);
         }
--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -1,13 +1,25 @@
 package fi.iki.jka;
 
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
 
 public class JPhotoFrameTest {
+
+    @Mock
+    JPhotoCollection jPhotoCollection = mock(JPhotoCollection.class);
+
+
     @Test
-    public void placeholder() throws Exception {
+    public void testStartShowsErrorWithNoPhotosSelected() throws Exception {
+
+        JPhotoFrame jPhotoFrame = new JPhotoFrame("name",jPhotoCollection);
+
+
         assertThat(2, equalTo(2));
     }
 }
